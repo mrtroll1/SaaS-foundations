98459e0bcac26803a4720ed6e50859e8a8ea9ede
luka <luka.asfari@gmail.com>
2024-10-20 23:27:37 +0200

    Use decouple to manage environment vars

--- src/mysaas/settings.py
+++ src/mysaas/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-zq-v0zn^@=murr(*9yu+0pelv$w85k$%mn_ngr_e3+*aer(bib"
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DJANGO_DEBUG", cast=bool)
 
 ALLOWED_HOSTS = [
     ".railway.app" #https://saas.prod.railway.app
