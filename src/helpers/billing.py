843b91f523bcc4f36bfcd870a4a35eccf5b4eb1b
luka <luka.asfari@gmail.com>
2024-11-01 16:57:18 +0100

    correct a typo

--- src/helpers/billing.py
+++ src/helpers/billing.py
@@ -5,7 +5,7 @@ from . import date_utils
 
 DJANGO_DEBUG=config('DJANGO_DEBUG', default=False, cast=bool)
 STRIPE_SECRET_KEY=config('STRIPE_SECRET_KEY', default=None, cast=str)
-STRIPE_TEST_OVERRIDE=config('STRIPE_TEST_OVERRIDE', dafault=False, cast=bool)
+STRIPE_TEST_OVERRIDE=config('STRIPE_TEST_OVERRIDE', default=False, cast=bool)
 
 if 'sk_test' in STRIPE_SECRET_KEY and not DJANGO_DEBUG and not STRIPE_TEST_OVERRIDE:
     raise ValueError("Invalid stripe key for prod")
