8e75ae74905137b4d2b70679e5c86272955fc940
luka <luka.asfari@gmail.com>
2024-11-01 12:51:38 +0100

    update booking form

--- src/bookings/forms.py
+++ src/bookings/forms.py
@@ -14,12 +14,20 @@ class BookingForm(forms.Form):
         widget=forms.TextInput(attrs={
             'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500',
             'placeholder': 'Enter your phone number',
-            'required': 'required'
+        })
+    )
+
+    banya = forms.ChoiceField(
+        choices=[('regular3', 'Regular 3 hours'), ('regular2', 'Regular 2 hours'), ('premium3', 'Premium 3 hours'), ('premium2', 'Premium 2 hours')],
+        label="Banya",
+        widget=forms.Select(attrs={
+            'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500',
         })
     )
 
     num_guests = forms.ChoiceField(
         choices=[(1, '1'), (2, '2'), (3, '3'), (4, '4')],
+        label="Number of guests",
         widget=forms.Select(attrs={
             'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500',
         })
@@ -37,6 +45,6 @@ class BookingForm(forms.Form):
         widget=forms.Textarea(attrs={
             'class': 'block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500',
             'placeholder': 'Your comment here',
-            'rows': '8'
+            'rows': '4'
         })
     )
